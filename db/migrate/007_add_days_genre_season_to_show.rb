9b1ecae696f55e73acf852be827fbd62ea50e2c6
Trish Ogen <patriciaogen@gmail.com>
2019-07-21 15:06:25 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_days_genre_season_to_show.rb
+++ db/migrate/007_add_days_genre_season_to_show.rb
@@ -3,4 +3,5 @@ class AddDayGenreSeasonToShow < ActiveRecord::Migration[5.1]
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
     add_column :shows, :season, :string
+  end
 end
