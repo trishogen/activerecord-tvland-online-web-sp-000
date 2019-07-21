233d8ed77d70b5baa125502d2bdc63a359d827b4
Trish Ogen <patriciaogen@gmail.com>
2019-07-21 14:44:49 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,11 @@ class Actor < ActiveRecord::Base
   def full_name
     self.first_name + ' ' + self.last_name
   end
+
+  def list_roles
+    self.characters.collect do |c|
+      c.name + " - " + c.show
+    end
+  end
+
 end
